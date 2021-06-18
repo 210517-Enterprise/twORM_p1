5ae5caf168f9cfd4765bd657c5c34081ba95809f
thomaspceci <thomas.ceci@revature.net>
2021-06-18 18:48:50 -0400

    fleshing out initial update statement

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,9 +1,22 @@
 package com.revature;
 
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+
+import com.revature.Meta.MetaConstructor;
+import com.revature.ObjSql.Updater;
+import com.revature.connection.ConnectionFactory;
+import com.revature.demo.Person;
 
 public class Driver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Updater u = Updater.getInstance();
+		MetaConstructor mc = MetaConstructor.getInstance();
+		mc.addModel(Person.class);
+		Person p1 = new Person("Billy", 10);
+		
+		u.updateObject(p1, null);
 		
 	}
 }
--- src/main/java/com/revature/ObjSql/Updater.java
+++ src/main/java/com/revature/ObjSql/Updater.java
@@ -1,6 +1,16 @@
 package com.revature.ObjSql;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.log4j.Logger;
+
+import com.revature.Meta.MetaConstructor;
+import com.revature.Meta.MetaModel;
 
 public class Updater extends Genericer {
 	
@@ -14,5 +24,37 @@ public class Updater extends Genericer {
 	
     public static Updater getInstance() {
         return ObjUpd;
+    }
+
+	protected String getColumnsAndValues(final HashMap<String, Method> getters, Object obj) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		StringBuilder sb = new StringBuilder();
+		for (Map.Entry<String, Method> getter : getters.entrySet()) {
+			sb.append(getter.getKey() + " = " + getter.getValue().invoke(obj).toString() + " , ");
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
+	}
+
+    public boolean updateObject(final Object obj, final Connection conn) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+//		try {
+            final MetaModel<?> model                           = MetaConstructor.getInstance().getModels().get(obj.getClass().getSimpleName());
+            final HashMap<String,Method> getters               = model.getGetters();
+            final String pkValue							   = getters.get(model.getPrimary_key_name()).invoke(obj).toString();
+            final String columns                               = getColumnsAndValues(getters, obj);
+            final String sql                                   = "UPDATE " + model.getEntity() + " SET " + columns + " WHERE " + model.getPrimary_key_name() + " = " + pkValue + ";";
+            System.out.println(sql);
+//          final PreparedStatement pstmt                      = conn.prepareStatement(sql);
+//			pstmt.execute();
+			return true;
+//			
+//			/*
+//			 * UPDATE THE CACHE SOMEHOW
+//			 */
+//			
+//		} catch (SQLException e) {
+//			log.error("Failed to update DB",e);
+//			return false;
+//		}
     }
 }
