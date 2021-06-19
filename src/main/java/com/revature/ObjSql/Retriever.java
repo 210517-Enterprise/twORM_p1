48f615045877db03d8c2eeb11a6d0c11823d5dd2
FBThornton <97fthornton@gmail.com>
2021-06-19 02:07:29 -0400

    Added Retriever functions

--- src/main/java/com/revature/ObjSql/Retriever.java
+++ src/main/java/com/revature/ObjSql/Retriever.java
@@ -1,6 +1,21 @@
 package com.revature.ObjSql;
 
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
+
 import org.apache.log4j.Logger;
+
+import com.revature.Meta.MetaConstructor;
+import com.revature.Meta.MetaModel;
 
 public class Retriever extends Genericer {
 
@@ -15,4 +30,75 @@ public class Retriever extends Genericer {
     public static Retriever getInstance() {
         return ObjRet;
     }
+    
+    public Optional<List<Object>> getAllEntity(Class<?> clazz,  Connection c) {
+    	
+    	String sql = "SELECT * FROM " + clazz.getSimpleName();
+    	try {
+			Statement stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			return resultSetToList(rs, clazz);
+			
+		} catch (SQLException e) {
+			log.error("Failed to get all entities for " + clazz.getSimpleName(), e);
+		}
+    	
+    	return Optional.empty();
+    }
+
+    private Optional<List<Object>> resultSetToList(ResultSet rs, Class<?> clazz) {
+    	
+    	try {
+    		List<Object> res = new ArrayList<>();
+    		MetaModel<?> model = MetaConstructor.getInstance().getModels().get(clazz.getSimpleName());
+    		
+    		// Iterate over all results
+    		while(rs.next()) {
+    			Object obj = model.getConstructor().newInstance();
+    			
+    			// Get Setters of the model
+    			Set<Map.Entry<Method, String[]>> setters = model.getSetters().entrySet();
+    			
+    			// For each setter convert the SQL type to Java
+    			for(Map.Entry<Method, String[]> s : setters) {
+    				setField(obj, s.getKey(), rs, s.getValue());
+    			}
+    			
+    			// Add object
+    			res.add(obj);
+    		}
+    		
+    		if(res.isEmpty()) {
+    			return Optional.empty();
+    		}
+    		
+    		return Optional.of(res);
+    		
+    	} catch (Exception e) {
+    		log.error("Error in converting ResultSet to List", e);
+    	}
+    	
+    	return Optional.empty();
+    }
+ 
+    private void setField(Object obj, Method method, ResultSet rs, String[] fields) {
+    	String type = fields[1];
+	    // fields contains {<column name>, <simple type name>}
+    	// TODO add more types
+    	try {
+    		if(type.equals("String")) {
+	    		method.invoke(obj, rs.getString(fields[0]));
+	    	} else if (type.equals("int")){
+	    		method.invoke(obj, rs.getInt(fields[0]));
+	    	} else if (type.equals("double")) {
+	    		method.invoke(obj, rs.getDouble(fields[0]));
+	    	} else if (type.equals("BigDecimal")) {
+	    		method.invoke(obj, rs.getBigDecimal(fields[0]));
+	    	}
+	    } catch (Exception e) {
+	    	log.error("Error in setting fields when retrieving", e);
+	    }
+    }
+    
 }
--- src/main/java/com/revature/testDriver.java
+++ src/main/java/com/revature/testDriver.java
@@ -1,31 +1,33 @@
 package com.revature;
 
-import com.revature.demo.Person;
-
 import java.sql.Connection;
+import java.util.List;
 
 import com.revature.Meta.MetaConstructor;
-import com.revature.ObjSql.Inserter;
-import com.revature.connection.*;
+import com.revature.ObjSql.Retriever;
+import com.revature.connection.ConnectionFactory;
+import com.revature.demo.Person;
 
 public class testDriver {
 
 	public static void main(String[] args) {
 		
-		Person p1 = new Person("Billy", 10);
-		Person p2 = new Person("Jan", 24);
-		Person p3 = new Person("Quincy", 177);
+//		Person p1 = new Person("Billy", 10);
+//		Person p2 = new Person("Jan", 24);
+//		Person p3 = new Person("Quincy", 177);
+//		
+//		Inserter i = Inserter.getInstance();
 		
-		Inserter i = Inserter.getInstance();
+		Connection c = ConnectionFactory.getInstance().getConnection();
 		
 		MetaConstructor mc = MetaConstructor.getInstance();
 		mc.addModel(Person.class);
 		
-		Connection c = ConnectionFactory.getInstance().getConnection();
-
-		i.saveObject(p1, c);
-		i.saveObject(p2, c);
-		i.saveObject(p3, c);
+		Retriever r = Retriever.getInstance();
+		List<Object> l = r.getAllEntity(Person.class, c).get();
+		
+		l.forEach(i -> System.out.println(i));
+		
 	}
 		
 }
