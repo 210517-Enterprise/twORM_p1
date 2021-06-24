3eb0a14097c45276229cdcf939282482ace8ea40
thomaspceci <thomas.ceci@revature.net>
2021-06-24 14:22:29 -0400

    testing Crime

--- twORM/src/main/java/com/revature/Driver.java
+++ twORM/src/main/java/com/revature/Driver.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Optional;
 
+import com.revature.demo.Crime;
 import com.revature.demo.Person_Two;
 import com.revature.twORM.TwORM;
 
@@ -13,26 +14,23 @@ public class Driver {
 	public static void main(String[] args)
 			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 
-		Person_Two tom = new Person_Two("Tom", 28);
-		Person_Two james = new Person_Two("James", 33);
-		Person_Two brandon = new Person_Two("Brandon", 27);
+		Crime arson = new Crime("Arson", "Burning Buildings", 5);
+		Crime assault = new Crime("Assault", "Threatening Violence", 10);
+		Crime battery = new Crime("Battery", "Commiting Violent Acts", 15);
 		
 		final TwORM t = TwORM.getInstance();
 		t.disableAutoCommit();
-		t.addClass(Person_Two.class);
+		t.addClass(Crime.class);
 		t.setTransactionLevel();
 		
-		t.addObjectToDb(tom);
-		t.addObjectToDb(james);
+		t.addObjectToDb(arson);
+		t.addObjectToDb(assault);
 		t.commitChanges();
-		
-		t.addObjectToDb(brandon);
-		t.abortChanges();
+		t.addObjectToDb(battery);
 		
 		HashMap<Class<?>, HashSet<Object>> cache = t.getCache();
-		System.out.println(cache.get(Person_Two.class));
-		
-		System.out.println(t.getByPK(Person_Two.class, 1));
+		System.out.println(cache.get(Crime.class));
+		System.out.println(t.getByPK(Person_Two.class, "Battery"));
 		
 		
 		}
