0597f6688c0feebf325061fe58718ee740a1c763
FBThornton <97fthornton@gmail.com>
2021-06-25 14:36:28 -0400

    Added annotations

--- PersonHolderDemo/src/main/java/com/revature/Models/Person.java
+++ PersonHolderDemo/src/main/java/com/revature/Models/Person.java
@@ -1,12 +1,21 @@
 package com.revature.Models;
 
+import com.revature.annotations.*;
+
+@Entity(name="person")
 public class Person {
 
+	@PrimaryKey(name="id", isSerial=true)
 	private int id;
+	@Column(name="first_name")
 	private String firstName;
+	@Column(name="last_name")
 	private String lastName;
+	@Column(name="job")
 	private String Job;
+	@Column(name="age")
 	private int age;
+	@Column(name="salary")
 	private double salary;
 	
 	public Person() {
@@ -31,50 +40,62 @@ public class Person {
 		this.salary = salary;
 	}
 
+	@Getter(name="id")
 	public int getId() {
 		return id;
 	}
 
+	@Setter(name="id")
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	@Getter(name="first_name")
 	public String getFirstName() {
 		return firstName;
 	}
 
+	@Setter(name="first_name")
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+	@Getter(name="last_name")
 	public String getLastName() {
 		return lastName;
 	}
 
+	@Setter(name="last_name")
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	@Getter(name="job")
 	public String getJob() {
 		return Job;
 	}
 
+	@Setter(name="job")
 	public void setJob(String job) {
 		Job = job;
 	}
 
+	@Getter(name="age")
 	public int getAge() {
 		return age;
 	}
 
+	@Setter(name="age")
 	public void setAge(int age) {
 		this.age = age;
 	}
 
+	@Getter(name="salary")
 	public double getSalary() {
 		return salary;
 	}
 
+	@Setter(name="salary")
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
